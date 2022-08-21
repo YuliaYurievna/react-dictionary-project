88c97168301f169c8d39db5aa87d4f94ab780c13
YuliaYurievna <104911480+YuliaYurievna@users.noreply.github.com>
2022-08-21 11:21:13 +0300

    Added common tag for сomponent Synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,13 +4,16 @@ import './Synonyms.css';
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className='Synonyms'>
-        {props.synonyms.map(function(synonym, index) {
-          return (
-            <li key={index}>{synonym}</li>
-          );
-        })}
-      </ul>
+      <div className='Synonyms'>
+        <strong>Similar:</strong>
+        <ul>
+          {props.synonyms.map(function(synonym, index) {
+            return (
+              <li key={index}>{synonym}</li>
+            );
+          })}
+        </ul>
+      </div>
     );
 
   } else {
